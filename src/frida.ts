e1f6ac822730404c1ef3795eccef51cbbe04ee93
Huan (李卓桓) <zixia@zixia.net>
2021-06-25 01:14:51 +0800

    add frida typings and tsd tests

--- src/frida.ts
+++ src/frida.ts
@@ -1,5 +1,79 @@
+/**
+ * Sidecar frida
+ *
+ * Huan <zixia@zixia.net>, June 25, 2021
+ *  https://github.com/huan/sidecar
+ */
 export * from 'frida'
 export {
   ScriptMessageHandler,
   ScriptDestroyedHandler,
 }                           from 'frida/dist/script'
+
+/**
+ * Frida Target
+ *
+ *  number:
+ *    - 0x1234
+ *
+ *  string:
+ *    - lib: 'open'
+ *    - ObjC: '- connection:didReceiveData:'
+ */
+export type TargetType =  number
+                        | string
+
+/**
+ * NativeFunction
+ *  https://frida.re/docs/javascript-api/#nativefunction
+ */
+export type NativeType =  'void'
+                        | 'pointer'
+                        | 'int'
+                        | 'uint'
+                        | 'long'
+                        | 'ulong'
+                        | 'char'
+                        | 'uchar'
+                        | 'size_t'
+                        | 'ssize_t'
+                        | 'float'
+                        | 'double'
+                        | 'int8'
+                        | 'uint8'
+                        | 'int16'
+                        | 'uint16'
+                        | 'int32'
+                        | 'uint32'
+                        | 'int64'
+                        | 'uint64'
+                        | 'bool'
+
+/**
+ * NativePointer
+ *  https://frida.re/docs/javascript-api/#nativepointer
+ */
+export type PointerType = 'Pointer'
+                        | 'S8'
+                        | 'U8'
+                        | 'S16'
+                        | 'U16'
+                        | 'S32'
+                        | 'U32'
+                        | 'Short'
+                        | 'UShort'
+                        | 'Int'
+                        | 'UInt'
+                        | 'Float'
+                        | 'Double'
+                        | 'S64'
+                        | 'U64'
+                        | 'Long'
+                        | 'ULong'
+                        | 'ByteArray'
+                        | 'CString'
+                        | 'Utf8String'
+                        | 'Utf16String'
+                        | 'AnsiString'
+
+// export type DataType = [NativeType, ...(undefined | PointerType)[]]
