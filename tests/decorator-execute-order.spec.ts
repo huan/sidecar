4ff9df91dd615bd4758e31a642ff37543e65317d
Huan (李卓桓) <zixia@zixia.net>
2021-06-25 04:05:17 +0800

    clearer decorator order test

--- tests/decorator-execute-order.spec.ts
+++ tests/decorator-execute-order.spec.ts
@@ -10,38 +10,38 @@ import 'reflect-metadata'
 test('decorator execute order', async t => {
   const orderList = [] as string[]
 
-  const decorateClass    = (..._: any[]) => { orderList.push('class') }
-  const decorateMethod   = (..._: any[]) => { orderList.push('method') }
-  const decorateParam    = (..._: any[]) => { orderList.push('param') }
-  const decorateProperty = (..._: any[]) => { orderList.push('property') }
+  const decorateClass    = (target: any, ..._: any[]) => { orderList.push('class:' + target.name) }
+  const decorateMethod   = (_: any, key: string, ..._args: any[]) => { orderList.push('method:' + key) }
+  const decorateParam    = (_: any, key: string, index: number) => { orderList.push('param:' + key + '/' + index) }
+  const decorateProperty = (_: any, key: string) => { orderList.push('property:' + key) }
 
   @decorateClass
   class Test {
 
-    @decorateProperty _: any
-    @decorateProperty _2: any
+    @decorateProperty _prop1: any
+    @decorateProperty _prop2: any
 
-    @decorateMethod method (
-      @decorateParam _: any,
+    @decorateMethod method1 (
+      @decorateParam _arg: any,
     ) {}
 
     @decorateMethod method2 (
-      @decorateParam _: any,
-      @decorateParam _2: any,
+      @decorateParam _arg1: any,
+      @decorateParam _arg2: any,
     ) {}
 
   }
   void Test
 
   const EXPECTED_ORDER_LIST = [
-    'property',
-    'property',
-    'param',
-    'method',
-    'param',
-    'param',
-    'method',
-    'class',
+    'property:_prop1',
+    'property:_prop2',
+    'param:method1/0',
+    'method:method1',
+    'param:method2/1',
+    'param:method2/0',
+    'method:method2',
+    'class:Test',
   ]
   t.deepEqual(orderList, EXPECTED_ORDER_LIST, 'should get the expected execute order of decorators')
 })
