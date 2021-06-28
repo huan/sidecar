b83447d35a8bd7c1e2717f4dff85e4447405c5a0
Huan (李卓桓) <zixia@zixia.net>
2021-06-28 16:03:18 +0800

    refine tests

--- src/decorators/call/update-rpc-descriptor.spec.ts
+++ src/decorators/call/update-rpc-descriptor.spec.ts
@@ -19,33 +19,31 @@ test('update & get call target metadata', async t => {
 
     script = {
       exports: {
-        method: () => AFTER_VALUE,
+        testMethod: () => AFTER_VALUE,
       },
     }
 
-    method () {
-      return Ret()
-    }
+    testMethod () { return Ret() }
 
   }
 
   const test = new Test()
 
-  const descriptor = Reflect.getOwnPropertyDescriptor(Test.prototype, 'method')
+  const descriptor = Reflect.getOwnPropertyDescriptor(Test.prototype, 'testMethod')
   const beforeValue = await descriptor?.value()
   t.equal(beforeValue, RET_SYMBOL, 'should get RET_SYMBOL before update rpc method')
 
   const rpcDescriptor = updateRpcDescriptor(
     Test,
-    'method',
+    'testMethod',
     descriptor!,
   )
 
   const rpcValue = await rpcDescriptor.value.bind(test)()
   t.equal(rpcValue, AFTER_VALUE, 'should get AFTER_VALUE from rpcValue')
 
-  Object.defineProperty(Test.prototype, 'method', rpcDescriptor)
-  const ret = await test.method()
+  Object.defineProperty(Test.prototype, 'testMethod', rpcDescriptor)
+  const ret = await test.testMethod()
   t.equal(ret, AFTER_VALUE, 'should get a updated method return value')
 
   await new Promise(setImmediate)
@@ -75,5 +73,5 @@ test('method to be proxyed must retur "Ret()"', async t => {
   )
   await new Promise(setImmediate)
 
-  t.true((Test as any)[CALL_RET_ERROR], 'should trigger error if the method does not return "Ret()"')
+  t.true((Test as any)[CALL_RET_ERROR], 'should trigger error if the method does not return "Ret()" (We can safely ignore the Error message above this test)')
 })
