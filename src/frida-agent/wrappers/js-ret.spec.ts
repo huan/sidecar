6ae9d4ceabf8d25b2d2450c3df468aeeaa8a5bc2
Huan (李卓桓) <zixia@zixia.net>
2021-06-29 19:00:41 +0800

    add jsRet & remove wrapper.ts

---
+++ src/frida-agent/wrappers/js-ret.spec.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env ts-node
+import { test }  from 'tstest'
+
+import { getSidecarViewFixture } from '../../../tests/fixtures/sidecar-view.fixture'
+import { jsRet } from './js-ret'
+
+test('jsRet()', async t => {
+  const SIDECAR_VIEW = getSidecarViewFixture()
+
+  const nativeFunctionList = SIDECAR_VIEW.nativeFunctionList
+
+  const EXPECTED_RET_LIST = [
+    'ret.readPointer().readInt()',
+    'ret.readPointer().readUtf8String()',
+  ]
+
+  const result = nativeFunctionList
+    .map(x => jsRet.call(x))
+
+  // console.log(result)
+  t.deepEqual(result, EXPECTED_RET_LIST, 'should wrap the ret correct')
+})
---
+++ src/frida-agent/wrappers/js-ret.ts
@@ -0,0 +1,30 @@
+import { SidecarFunctionDescription } from '../views/sidecar-view'
+
+function jsRet (this: SidecarFunctionDescription) {
+  const typeChain = this.retType
+  if (!typeChain) {
+    throw new Error('no .retType found in SidecarFunctionDescription context!')
+  }
+
+  const [nativeType, ...pointerTypeList] = typeChain
+  // console.log(nativeType, pointerTypeList)
+
+  const readChain = [
+    'ret',
+  ]
+
+  if (nativeType === 'pointer') {
+    readChain.push(
+      '.readPointer()'
+    )
+    for (const pointerType of pointerTypeList) {
+      readChain.push(
+        `.read${pointerType}()`
+      )
+    }
+  }
+
+  return readChain.join('')
+}
+
+export { jsRet }
