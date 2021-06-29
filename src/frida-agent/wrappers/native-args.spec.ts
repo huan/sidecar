24d0b596c567a344621dbc8dbbe6df2080c3ec42
Huan (李卓桓) <zixia@zixia.net>
2021-06-29 19:32:39 +0800

    add nativeArgs helper function

---
+++ src/frida-agent/wrappers/native-args.spec.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env ts-node
+import { test }  from 'tstest'
+
+import { getSidecarViewFixture } from '../../../tests/fixtures/sidecar-view.fixture'
+
+import {
+  nativeArgs,
+}                       from './native-args'
+
+test('nativeArgs()', async t => {
+
+  const fixture = getSidecarViewFixture()
+
+  console.log(fixture.nativeFunctionList.length)
+  const result = fixture.nativeFunctionList.map(x => nativeArgs.call(x))
+
+  const EXPECTED_RESULT = [
+    '[ anotherCall_NativeArg_0, anotherCall_NativeArg_1 ]',
+    '[ testMethod_NativeArg_0, testMethod_NativeArg_1 ]',
+  ]
+  t.deepEqual(result, EXPECTED_RESULT, 'should list the native arg names correctly.')
+})
---
+++ src/frida-agent/wrappers/native-args.ts
@@ -0,0 +1,22 @@
+import { SidecarFunctionDescription } from '../views/sidecar-view'
+import { nativeArgName } from './name-helpers'
+
+function nativeArgs (this: SidecarFunctionDescription) {
+  const name = this.name
+  const paramTypeList = this.paramTypeList
+  if (!Array.isArray(paramTypeList)) {
+    throw new Error('Can not found .paramTypeList in SidecarFunctionDescription!')
+  }
+
+  const nativeArgNameList = []
+
+  for (let i = 0; i < paramTypeList.length; i++) {
+    nativeArgNameList.push(
+      nativeArgName(name, i)
+    )
+  }
+
+  return '[ ' + nativeArgNameList.join(', ') + ' ]'
+}
+
+export { nativeArgs }
