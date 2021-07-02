17871133fc3c212ccb5f6633fec71e3dcc6e23a7
Huan (李卓桓) <zixia@zixia.net>
2021-07-02 23:55:28 +0800

    add bundle ts file util

---
+++ src/dump/bundle-ts-file.ts
@@ -0,0 +1,23 @@
+import browserify from 'browserify'
+import tsify from 'tsify'
+
+const options = {
+  noImplicitAny          : true,
+  target                 : 'es6',   // https://github.com/TypeStrong/tsify#es2015-formerly-known-as-es6
+} as any
+
+const bundleTsFile = (file: string) => new Promise((resolve, reject) => {
+  const stream = browserify()
+    .add(file)
+    .plugin(tsify, options)
+    .bundle()
+
+  const chunkList = [] as Buffer[]
+
+  stream
+    .on('data', chunk => chunkList.push(chunk))
+    .on('error', reject)
+    .on('end', () => resolve(Buffer.concat(chunkList).toString()))
+})
+
+export { bundleTsFile }
