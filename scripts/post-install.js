813d3f94ad2114917a45e24e8c896e70ae4dbdf8
Huan LI (李卓桓) <zixia@zixia.net>
2021-08-05 13:35:10 +0800

    fix cwd for frida build release folder

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -2,7 +2,7 @@
 const { spawnSync } = require('child_process')
 const path          = require('path')
 const fs            = require('fs')
-const pkgUp         = require('pkg-up')
+const pkgDir        = require('pkg-dir')
 
 function needInstall () {
   try {
@@ -16,28 +16,32 @@ function needInstall () {
 async function reinstall () {
   console.error('Sidecar: checking frida installation (frida_binding.node) failed, try to reinstall with cdn mirror...')
 
-  const env = {
-    ...process.env,
-    npm_config_frida_binary_host_mirror: 'https://cdn.chatie.io/mirrors/github.com/frida/frida/releases/download',
-  }
-
-  const pkgRoot = await pkgUp()
+  const pkgRoot = await pkgDir(__dirname)
   if (!pkgRoot) {
     throw new Error('no package.json found')
   }
 
-  const cwdCurrent  = path.resolve(pkgRoot, 'node_modules/frida')
-  const cwdParent   = path.resolve(pkgRoot, '../node_modules/frida')
+  const innerCwd = path.resolve(pkgRoot, 'node_modules/frida')
+  const outerCwd = path.resolve(pkgRoot, '../frida')
 
-  const cwd = fs.existsSync(cwdCurrent)
-    ? cwdCurrent
-    : cwdParent
+  const cwd = fs.existsSync(innerCwd) ? innerCwd
+    : fs.existsSync(outerCwd) ? outerCwd
+      : undefined
+
+  if (!cwd) {
+    throw new Error('can not find "node_modules/frida"')
+  }
 
   const args = [
     'prebuild-install',
     '--tag-prefix',
     '',
   ]
+
+  const env = {
+    ...process.env,
+    npm_config_frida_binary_host_mirror: 'https://cdn.chatie.io/mirrors/github.com/frida/frida/releases/download',
+  }
 
   const ret = await spawnSync(
     'npx',
