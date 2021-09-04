69bb1ca88dc1e22d354d4943e219a83b0d1b7a51
Huan (李卓桓) <zixia@zixia.net>
2021-09-05 04:55:45 +0800

    remove tsd to protect the typescript / tsc (#18)

--- src/frida.spec.ts
+++ src/frida.spec.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env -S node --no-warnings --loader ts-node/esm
 
 import { test }       from 'tstest'
-import { expectType } from 'tsd'
 
 import type {
   ScriptMessageHandler,
@@ -14,32 +13,43 @@ import {
   TargetPayloadRaw,
 }                           from './function-target.js'
 
+/**
+ * Huan(202109): #18 - `dts` package conflict node_modules/.bin/tsc version
+ *  https://github.com/huan/sidecar/issues/18
+ *
+ *  Testing static types in TypeScript
+ *    https://2ality.com/2019/07/testing-static-types.html
+ */
+type AssertEqual<T, Expected> =
+  T extends Expected
+  ? (Expected extends T ? true : never)
+  : never;
+
 test('PointerType typing', async t => {
   type EXPECTED_TYPE = 'Pointer' | 'Int' | 'Utf8String'
   type T = Extract<PointerType, EXPECTED_TYPE>
-  const type: T = {} as any
-  expectType<EXPECTED_TYPE>(type)
-  t.pass('PointerType should be typing right')
+  const typeTest: AssertEqual<T, EXPECTED_TYPE> = true
+  t.ok(typeTest, 'PointerType should be typing right')
 })
 
 test('NativeType typing', async t => {
   type EXPECTED_TYPE = 'void' | 'pointer' | 'int'
   type T = Extract<NativeType, EXPECTED_TYPE>
-  const type: T = '' as any
-  expectType<EXPECTED_TYPE>(type)
-  t.pass('NativeType should be typing right')
+  const typeTest: AssertEqual<T, EXPECTED_TYPE> = true
+  t.ok(typeTest, 'NativeType should be typing right')
 })
 
 test('TargetType typing', async t => {
-  const type: TargetPayloadRaw = '' as any
-  expectType<number | string>(type)
-  t.pass('TargetType should be typing right')
+  type EXPECTED_TYPE = number | string
+  const typeTest: AssertEqual<TargetPayloadRaw, EXPECTED_TYPE> = true
+  t.ok(typeTest, 'TargetType should be typing right')
 })
 
 test('ScriptMessageHandler typing', async t => {
+  type EXPECTED_TYPE = Buffer | null
   const handler: Parameters<ScriptMessageHandler>[1] = {} as any
-  expectType<Buffer | null>(handler)
-  t.pass('ScriptMessageHandler should be typing right')
+  const typeTest: AssertEqual<typeof handler, EXPECTED_TYPE> = true
+  t.ok(typeTest, 'ScriptMessageHandler should be typing right')
 })
 
 test('normalizeFunctionTarget()', async t => {
